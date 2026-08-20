85d82b5ebf9faaa89604d6fbc4cdfa85121a7819
tam95supra-source <tam95.supra@gmail.com>
2026-08-20 17:36:12 +0700

    feat(s36): wire performance patch into Android transform chain

--- tools/apply_s31b_preserve_admin_audit.py
+++ tools/apply_s31b_preserve_admin_audit.py
@@ -28,3 +28,4 @@ runpy.run_path(str(ROOT/'tools/apply_s34b_compile_hotfix.py'),run_name='__main__
 runpy.run_path(str(ROOT/'tools/apply_s34c_site1291_local_report.py'),run_name='__main__')
 runpy.run_path(str(ROOT/'tools/apply_s34d_compile_fixes.py'),run_name='__main__')
 runpy.run_path(str(ROOT/'tools/apply_s35_owner_ui_history_consistency_wrapper.py'),run_name='__main__')
+runpy.run_path(str(ROOT/'tools/apply_s36_perf_history_report_service.py'),run_name='__main__')
