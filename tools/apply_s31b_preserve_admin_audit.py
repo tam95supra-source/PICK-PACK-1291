5e88ba9ce9ea883d9127a552bad5317138fb267b
tam95supra-source <tam95.supra@gmail.com>
2026-08-20 16:35:39 +0700

    android: chain S35 owner UI history fixes

--- tools/apply_s31b_preserve_admin_audit.py
+++ tools/apply_s31b_preserve_admin_audit.py
@@ -27,3 +27,4 @@ runpy.run_path(str(ROOT/'tools/apply_s34_owner_six_requests.py'),run_name='__mai
 runpy.run_path(str(ROOT/'tools/apply_s34b_compile_hotfix.py'),run_name='__main__')
 runpy.run_path(str(ROOT/'tools/apply_s34c_site1291_local_report.py'),run_name='__main__')
 runpy.run_path(str(ROOT/'tools/apply_s34d_compile_fixes.py'),run_name='__main__')
+runpy.run_path(str(ROOT/'tools/apply_s35_owner_ui_history_consistency.py'),run_name='__main__')
