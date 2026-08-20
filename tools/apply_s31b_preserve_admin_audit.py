37775408b0d0e7409bee02f409c87421666ac3a9
tam95supra-source <tam95.supra@gmail.com>
2026-08-20 20:27:13 +0700

    build: chain S39 Android hotfix after S38

--- tools/apply_s31b_preserve_admin_audit.py
+++ tools/apply_s31b_preserve_admin_audit.py
@@ -32,3 +32,4 @@ runpy.run_path(str(ROOT/'tools/apply_s36_perf_history_report_service.py'),run_na
 runpy.run_path(str(ROOT/'tools/apply_s36b_compile_hotfix.py'),run_name='__main__')
 runpy.run_path(str(ROOT/'tools/apply_s37_move_service_telemetry_to_sync.py'),run_name='__main__')
 runpy.run_path(str(ROOT/'tools/apply_s38_attendance_ui.py'),run_name='__main__')
+runpy.run_path(str(ROOT/'tools/apply_s39_employee_session_history.py'),run_name='__main__')
